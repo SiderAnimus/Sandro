41768cebb3bcab3c6f742893f83f42f816bd441d
Matheus_FSL <matheusf.slima190@gmail.com>
2025-10-12 20:49:20 +0000

    Atua

--- curriculo2.py
+++ curriculo2.py
@@ -2,65 +2,59 @@ def gravaInformacao():
     pessoa=open('pessoa.txt','a')
     data=open('data.txt','a')
     fone=open('fone.txt','a')
-    
     nome=input('Informe o seu nome: ')
     idade=input('Digite sua idade: ')
     telefone=input('Digite seu telefone: ')
-    
     pessoa.write(nome+'\n')
     data.write(idade+'\n')
     fone.write(telefone+'\n')
-    
     pessoa.close()
     data.close()
     fone.close()
 
 def gravaHabilidade():
-    habilidade=open('habilidade.txt','a')
-    
-    habil=input('Quais são suas habilidades: ')
-    habilidade.write(habil+'\n')
-    
-    habilidade.close()
-    
+    resposta='s'
+    i=0
+    while resposta == 's' or resposta == 'S':
+        habilidade=open('habilidade.txt','a')
+        habil=input('Quais são suas habilidades: ')
+        habilidade.write(habil+'\n')
+        habilidade.close()
+        resposta = input('Deseja continuar (S/N)? ')
+    i=i+1
+    return i
+
 def gravaTrabalho():
-    trabalho=open('trabalho.txt','a')
+    resposta='s'
+    j=0
+    while resposta == 's' or resposta == 'S':
+        trabalho=open('trabalho.txt','a')
+        trab=input('Digite onde já trabalhou: ')
+        trabalho.write(trab+'\n')
+        trabalho.close()
+        resposta = input('Deseja continuar (S/N)? ')
+    j=j+1
+    return j
     
-    trab=input('Digite onde já trabalhou: ')
-    trabalho.write(trab+'\n')
+def criacaoCurriculo(m,n):
+    curriculo=open('curriculo.txt','w')
+    dat=open('data.txt','r')
+    da=dat.readlines()
     
-    trabalho.close()
+    curriculo.write('<!DOCTYPE html>\n')
+    curriculo.write('<html>\n')
+    curriculo.write('   <head>\n')
+    curriculo.write('       <style>\n')
+    curriculo.write('           *{\n')
+    curriculo.write('               backgroud-color:#00ffff\n')
+    curriculo.write('               font-family: Verdana, Geneva, Tahoma, sans-serif;\n')
+    curriculo.write('           }\n')
+    curriculo.write('       </style>\n')
+    curriculo.write('   </head>\n')
+    curriculo.write('   <body>\n')
+    curriculo.write('       <p>Idade:'+da+'</p>\n')
+    curriculo.write('   </body>\n')
+    curriculo.write('</html>\n')
     
-def criacaocurriculo():
-
-with open("pessoa.txt",'r') as pessoas:
-pessoa = pessoas.read()
-
- with open("curriculo.txt", 'w') as arquivo:
-
-html = [
-"<html>",
-"<body>",
-"<h1>",
-pessoa,
-"</h1>",
-"</body>",
-"</html>",
-
-
-]
-
-for i in html:
-arquivo.write(i)
-
-    
-resposta='s'
-resp='s'
-gravaInformacao()
-while resposta == 's' or resposta == 'S':
-    gravaHabilidade()
-    resposta = input('Deseja continuar (S/N)? ')
-while resp == 's' or resp == 'S':
-    gravaTrabalho()
-    resp = input('Deseja continuar (S/N)? ')
-criacaoCurriculo()
+    curriculo.close()
+    data.close()
